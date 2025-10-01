48a83904af7428d5ecdc7890b5adc83fe9f655e6
Mathivathana S <45286002+mathik2@users.noreply.github.com>
2025-10-01 20:20:27 +0530

    Formated

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+
+# Using default async_mode (gevent) which works on Python 3.13
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Relay offer
+# Relay WebRTC offer
 @socketio.on('offer')
 def handle_offer(data):
     emit('offer', data, broadcast=True, include_self=False)
 
-# Relay answer
+# Relay WebRTC answer
 @socketio.on('answer')
 def handle_answer(data):
     emit('answer', data, broadcast=True, include_self=False)
@@ -25,11 +26,10 @@ def handle_answer(data):
 def handle_ice_candidate(data):
     emit('ice-candidate', data, broadcast=True, include_self=False)
 
+# Relay hangup signal
 @socketio.on('hangup')
 def handle_hangup():
     emit('hangup', broadcast=True, include_self=False)
 
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
-
-
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
