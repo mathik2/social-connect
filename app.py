da6586941612f6845082927eef1992b9b95fd20a
Mathivathana S <45286002+mathik2@users.noreply.github.com>
2025-10-01 19:22:57 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Relay offer
+@socketio.on('offer')
+def handle_offer(data):
+    emit('offer', data, broadcast=True, include_self=False)
+
+# Relay answer
+@socketio.on('answer')
+def handle_answer(data):
+    emit('answer', data, broadcast=True, include_self=False)
+
+# Relay ICE candidates
+@socketio.on('ice-candidate')
+def handle_ice_candidate(data):
+    emit('ice-candidate', data, broadcast=True, include_self=False)
+
+@socketio.on('hangup')
+def handle_hangup():
+    emit('hangup', broadcast=True, include_self=False)
+
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+
+
